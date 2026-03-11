a3ffff4b7bac8e59627d553c851a5667987b57b3
Adoniss14 <liam.chevassus@epita.fr>
2026-03-11 02:35:38 +0100

    fix: fix errors in script

--- script.js
+++ script.js
@@ -6,27 +6,30 @@ async function loadBazaar()
     {
         console.log("Loading Hypixel Skyblock API at https://api.hypixel.net/v2/skyblock/bazaar");
         const response = await fetch(API_URL);
-        const data = await response.json;
+        const data = await response.json();
         console.log("Successfully loaded Hypixel Skyblock API");
 
         const products = data.products;
-        console.log(data.products);
+        console.log(products);
         const container = document.getElementById("bazaar-container");
 
         container.innerHTML = "";
 
+        const productsCount = Object.keys(products).length;
+        console.log(`Found ${productsCount} products on the Bazaar`)
+
         for (const itemId in products)
         {
             const item = products[itemId].quick_status;
-            console.log("Item id {itemId}: Name ${item.productId}, buy ${item.buyPrice}, sell ${item.sellPrice}");
+            console.log(`Item id ${itemId}: Name ${item.productId}, buy ${item.buyPrice}, sell ${item.sellPrice}`);
 
             const div = document.createElement("div");
             div.className = "bazaar-item";
 
             div.innerHTML = `
-                <strong>${itemId}itemId</strong><br>
-                Buy: {item.buyPrice.toFixed(2)<br>
-                Sell: {item.sellPrice.toFixed(2)}
+                <strong>${itemId}</strong><br>
+                Buy: ${item.buyPrice.toFixed(2)}<br>
+                Sell: ${item.sellPrice.toFixed(2)}
             `;
 
             container.appendChild(div);
