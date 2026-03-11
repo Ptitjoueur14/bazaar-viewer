ae5d33712bc3360f87ac9a0e9a361b888c390480
Adoniss14 <liam.chevassus@epita.fr>
2026-03-11 02:25:59 +0100

    feat: add products console log

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ async function loadBazaar()
         console.log("Successfully loaded Hypixel Skyblock API");
 
         const products = data.products;
+        console.log(data.products);
         const container = document.getElementById("bazaar-container");
 
         container.innerHTML = "";
@@ -17,6 +18,7 @@ async function loadBazaar()
         for (const itemId in products)
         {
             const item = products[itemId].quick_status;
+            console.log("Item id {itemId}: Name ${item.productId}, buy ${item.buyPrice}, sell ${item.sellPrice}");
 
             const div = document.createElement("div");
             div.className = "bazaar-item";
@@ -26,8 +28,6 @@ async function loadBazaar()
                 Buy: {item.buyPrice.toFixed(2)<br>
                 Sell: {item.sellPrice.toFixed(2)}
             `;
-
-            console.log("Item id {itemId}: Name ${item.productId}, buy ${item.buyPrice}, sell ${item.sellPrice}");
 
             container.appendChild(div);
         }
